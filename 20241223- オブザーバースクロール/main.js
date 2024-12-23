0246c9e2245cdab4d011ccec6b0ed62d8ee09bad
Nakayama <123441983+Shohei-Nakayama@users.noreply.github.com>
2024-12-23 19:05:11 +0900

    Add files via upload

---
+++ 20241223- オブザーバースクロール/main.js
@@ -0,0 +1,31 @@
+let imagesItems = [...document.querySelectorAll(".img-wrap")];
+console.log(imagesItems);
+let titles = [...document.querySelectorAll("h2")];
+let titleMessage = document.querySelector(".title");
+
+// 監視対象になった瞬間、activeを付加する関数
+let setItemActive = (entries) => {
+    console.log(entries);
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("active");
+        }else{
+            entry.target.classList.remove("active");
+        }
+    });
+};
+
+let options = {};
+
+// どこにいるのか監視する。そして特定の位置に来たら関数を呼ぶ。
+let observer = new IntersectionObserver(setItemActive, options);
+
+observer.observe(titleMessage);
+
+//ima-wrapは偶数と奇数で出現する場所が違う。そういう処理。
+imagesItems.map((item, index) => {
+    console.log(item, index);
+    item.children[0].style.backgroundImage = `url(./images/${index + 1}.jpg)`;
+    index % 2 == 0 ? (item.style.left = "55%") : (item.style.left = "5%");
+    observer.observe(item);
+});
